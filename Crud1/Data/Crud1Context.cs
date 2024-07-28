fc90ff85976e43348dbb79b6c0b1670b348142e3
Matheus Marinho <mt.mrnh.esp@gmail.com>
2024-07-28 12:49:56 -0300

    Criado o crud basico e a logica do vencimento dos pedidos

---
+++ Crud1/Data/Crud1Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crud1.Models;
+
+namespace Crud1.Data
+{
+    public class Crud1Context : DbContext
+    {
+        public Crud1Context (DbContextOptions<Crud1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crud1.Models.Pedido> Pedido { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pedido>()
+                .Property(p => p.Valor)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Crud1/Models/Pedido.cs
@@ -0,0 +1,26 @@
+﻿namespace Crud1.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal Valor { get; set; }
+
+        public DateTime Vencimento { get; set; }
+        public int PedidoStatus()
+        {
+            TimeSpan IntervaloDias = Vencimento.Subtract(DateTime.Now);
+            int Status = 2;
+            if (IntervaloDias.Days <= 0)
+            {
+                Status = 0;
+            }
+            else if (IntervaloDias.Days < 2)
+            {
+                Status = 1;
+            }
+
+            return Status;
+        }
+    }
+}
